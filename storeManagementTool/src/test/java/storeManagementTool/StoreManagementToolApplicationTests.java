1d8e3d5ae5ea6f67c4d7251cf16d6492fd12d826
Catalina07 <encea.catalina@yahoo.com>
2024-11-08 22:26:30 +0200

    set up base project with spring boot

---
+++ storeManagementTool/src/test/java/storeManagementTool/StoreManagementToolApplicationTests.java
@@ -0,0 +1,13 @@
+package storeManagementTool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreManagementToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
