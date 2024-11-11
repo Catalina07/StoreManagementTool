e5b425907669a1bdb797b3db29428507e1571070
Catalina07 <encea.catalina@yahoo.com>
2024-11-11 23:05:35 +0200

    first flow for product logic

--- storeManagementTool/src/main/java/storeManagementTool/Controllers/ProductController.java
+++ storeManagementTool/src/main/java/storeManagementTool/Controllers/ProductController.java
@@ -1,2 +1,53 @@
-package storeManagementTool.Controllers;public class ProductController {
+package storeManagementTool.Controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import storeManagementTool.Dtos.ProductEntityToDTO;
+import storeManagementTool.Dtos.ProductReqDTO;
+import storeManagementTool.Services.ProductService;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+
+    @Autowired
+    ProductService productService;
+
+    @GetMapping("/PostRequestBody")
+    public ProductReqDTO getRequestBody(){
+        return productService.getReq();
+    }
+
+    @GetMapping("/test")
+    public String testing(){
+        return "It works!";
+    }
+
+    @GetMapping("/{id}")
+    public ProductEntityToDTO getProductById(@PathVariable Long id){
+        return productService.getProductById(id);
+    }
+
+    @PostMapping(
+            consumes = {MediaType.APPLICATION_JSON_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE}
+    )
+//    TODO: @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<ProductEntityToDTO> createRestaurant(@RequestBody ProductReqDTO request) {
+        return new ResponseEntity<>(productService.createProduct(request), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+//    TODO: @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public void deleteProduct(@PathVariable Long id) {
+        productService.deleteProductById(id);
+    }
+
+
+
+
 }
--- storeManagementTool/src/main/java/storeManagementTool/Services/ProductService.java
+++ storeManagementTool/src/main/java/storeManagementTool/Services/ProductService.java
@@ -1,2 +1,69 @@
-package storeManagementTool.Services;public class ProductService {
+package storeManagementTool.Services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import storeManagementTool.Dtos.ProductEntityToDTO;
+import storeManagementTool.Dtos.ProductReqDTO;
+import storeManagementTool.Entities.Cart;
+import storeManagementTool.Entities.Product;
+import storeManagementTool.Mappers.ProductMapper;
+import storeManagementTool.Repositories.CartRepository;
+import storeManagementTool.Repositories.ProductRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @Autowired
+    CartRepository cartRepository;
+
+    @Autowired
+    ProductMapper productMapper;
+
+    public ProductReqDTO getReq(){
+        return new ProductReqDTO();
+    }
+
+    public List<ProductEntityToDTO> getProducts(){
+        return productRepository.findAll().stream()
+                .map(productMapper::entityToDTO)
+                .collect(Collectors.toList());
+    }
+
+    public ProductEntityToDTO getProductById(long id){
+        Optional<Product> product = productRepository.findById(id);
+
+        if(product.isPresent()){
+            return productMapper.entityToDTO(product.get());
+        }
+        else {
+            // TO DO add exceptions
+        }
+        return null;
+    }
+
+    public void deleteProductById(long id){
+        productRepository.deleteById(id);
+    }
+
+    public ProductEntityToDTO createProduct(ProductReqDTO productReqDTO){
+        Product product = new Product();
+
+        product.setName(productReqDTO.getName());
+        product.setDescription(productReqDTO.getDescription());
+        product.setAvailable(productReqDTO.isAvailable());
+        product.setPrice(productReqDTO.getPrice());
+        product.setQuantity(productReqDTO.getQuantity());
+
+        productRepository.save(product);
+
+        return productMapper.entityToDTO(product);
+    }
+
 }
