b69448e32ec9331c662b501aef1c84ebdab739ef
Catalina07 <encea.catalina@yahoo.com>
2024-11-11 23:29:31 +0200

    added working flow for product: add, view all products from database, delete  and update name/price/quantity/availability

--- storeManagementTool/src/main/java/storeManagementTool/Controllers/ProductController.java
+++ storeManagementTool/src/main/java/storeManagementTool/Controllers/ProductController.java
@@ -9,6 +9,8 @@ import storeManagementTool.Dtos.ProductEntityToDTO;
 import storeManagementTool.Dtos.ProductReqDTO;
 import storeManagementTool.Services.ProductService;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
@@ -37,7 +39,7 @@ public class ProductController {
             produces = {MediaType.APPLICATION_JSON_VALUE}
     )
 //    TODO: @PreAuthorize("hasRole('ROLE_ADMIN')")
-    public ResponseEntity<ProductEntityToDTO> createRestaurant(@RequestBody ProductReqDTO request) {
+    public ResponseEntity<ProductEntityToDTO> createProduct(@RequestBody ProductReqDTO request) {
         return new ResponseEntity<>(productService.createProduct(request), HttpStatus.CREATED);
     }
 
@@ -47,7 +49,15 @@ public class ProductController {
         productService.deleteProductById(id);
     }
 
+    @GetMapping()
+    public List<ProductEntityToDTO> getProducts(){
+        return productService.getProducts();
+    }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductEntityToDTO> updateProduct(@PathVariable Long id, @RequestBody ProductEntityToDTO request){
+        return new ResponseEntity<ProductEntityToDTO>(productService.updateProduct(id, request), HttpStatus.OK);
+    }
 
 
 }
--- storeManagementTool/src/main/java/storeManagementTool/Services/ProductService.java
+++ storeManagementTool/src/main/java/storeManagementTool/Services/ProductService.java
@@ -10,6 +10,7 @@ import storeManagementTool.Mappers.ProductMapper;
 import storeManagementTool.Repositories.CartRepository;
 import storeManagementTool.Repositories.ProductRepository;
 
+import java.awt.*;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -60,6 +61,23 @@ public class ProductService {
         product.setAvailable(productReqDTO.isAvailable());
         product.setPrice(productReqDTO.getPrice());
         product.setQuantity(productReqDTO.getQuantity());
+
+        productRepository.save(product);
+
+        return productMapper.entityToDTO(product);
+    }
+
+    public ProductEntityToDTO updateProduct(long id, ProductEntityToDTO request){
+        Optional<Product> optionalProduct = productRepository.findById(id);
+
+        // TODO: add case when we cannot find the product with the specific id; working now on the happy flow
+        Product product = optionalProduct.get();
+
+        product.setName(request.getName());
+        product.setQuantity(request.getQuantity());
+        product.setDescription(request.getDescription());
+        product.setAvailable(request.isAvailable());
+        product.setPrice(request.getPrice());
 
         productRepository.save(product);
 
